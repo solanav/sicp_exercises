6f0351177b4bde053469056f959bc866843463fe
solanav <solanav@qq.com>
2019-08-09 17:05:21 +0200

    Finished 1.7

--- 1_7.scm
+++ 1_7.scm
@@ -5,7 +5,14 @@ our procedure is useless.
 For very big numbers, the precission you need to satisfy this
 procedure is extremely high which makes it also useless in
 this case.
+
+This new procedure just compares the new and last guess and
+gives True only when the difference is small enough. This works
+well with big numbers: it keeps guessing until the changes are
+too small to be relevant and then gives you the square root.
+On numbers smaller than 0.001 it still does not work because
+the difference of two numbers smaller than 0.001 is always True.
 |#
 
-(define (good-enough? guess x)
-    (< (abs (- (* guess guess) x)) 0.001))
+(define (good-enough? last_guess new_guess)
+    (< (abs (- new_guess last_guess)) 0.001))
