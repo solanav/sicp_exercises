96e7db5e43b59852e2eb43c04ebd9d2459929e71
solanav <solanav@qq.com>
2019-08-09 17:16:02 +0200

    1.8

--- 1_8.scm
+++ 1_8.scm
@@ -1,24 +1,17 @@
 (define (square x) (* x x))
+(define (cube x) (* x x x))
 
-(define (sqrt-iter guess x)
+(define (cube-root-iter guess x)
   (if (good-enough? guess x)
       guess
-      (sqrt-iter (improve guess x)
+      (cube-root-iter (improve guess x)
                  x)))
 
-;;; Old procedure to improve a guess
-;;; (define (improve guess x)
-;;;   (average guess (/ x guess)))
-
-;;; New procedure based on Newton's method
 (define (improve guess x)
     (/ (+ (/ x (square guess)) (* 2 guess)) 3))
 
-(define (average x y)
-  (/ (+ x y) 2))
-
 (define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+  (< (abs (- (cube guess) x)) 0.001))
 
-(define (sqrt x)
-  (sqrt-iter 1.0 x))
+(define (cube-root x)
+    (cube-root-iter 1.0 x))
